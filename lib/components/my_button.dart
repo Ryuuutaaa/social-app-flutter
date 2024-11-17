ae945b2fef7ea762a9311f202a36b91b4460279a
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-18 01:58:43 +0800

    feat : add syle button

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final String text;
+  final void Function()? onTab;
+  const MyButton({super.key, required this.text, this.onTab});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: onTab,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: const EdgeInsets.all(25),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+      ),
+    );
   }
 }
