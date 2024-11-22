7c743b6f1ea7363c78a305e760184b72a16c5936
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-22 14:20:54 +0800

    feat : add show login and toogle betweenn

---
+++ lib/auth/login_or_register.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  // initial, show login page
+  bool showLoginPage = true;
+
+  // tooggle between login and register page
+  void togglePage() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
