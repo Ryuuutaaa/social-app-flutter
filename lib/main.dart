80177dbcf17d6152bca9ba4a62f09c424971c26e
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-23 11:07:50 +0800

    feat : add get started firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/auth/login_or_register.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/pages/register_page.dart';
 import 'package:flutter_application_1/theme/dark_mode.dart';
 import 'package:flutter_application_1/theme/light_mode.dart';
 
 import 'pages/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
