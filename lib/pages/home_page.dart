b3cda6c6267013e9bf56ea3536f43181ae2785a4
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-28 22:14:33 +0800

    feat : add fitur logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  // logout user
+  void logout() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
