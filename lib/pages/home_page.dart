6627dad6241c66a0e03862eadec856147103cfd4
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-29 09:38:26 +0800

    feat : add my drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,7 +17,7 @@ class HomePage extends StatelessWidget {
         title: Text(
           "Home",
         ),
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         actions: [
           // logout button
           IconButton(
@@ -25,6 +26,7 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
+      drawer: MyDrawer(),
     );
   }
 }
