6a721b1d6222ba9e480ed4e7b59fae2ea6039ad2
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-29 09:40:23 +0800

    feat : add drawer content

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -5,6 +5,8 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+    );
   }
 }
