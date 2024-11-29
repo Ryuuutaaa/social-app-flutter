4dd0a0c10b079cbeb8e3332fe230f55f96d08967
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-29 10:10:22 +0800

    feat : add route

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/auth/auth.dart';
 import 'package:flutter_application_1/auth/login_or_register.dart';
 import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/profile_page.dart';
 import 'package:flutter_application_1/pages/register_page.dart';
+import 'package:flutter_application_1/pages/user_page.dart';
 import 'package:flutter_application_1/theme/dark_mode.dart';
 import 'package:flutter_application_1/theme/light_mode.dart';
 
@@ -26,6 +29,12 @@ class MyApp extends StatelessWidget {
       theme: lightMode, // Tema terang
       darkTheme: darkMode, // Tema gelap
       themeMode: ThemeMode.system, // Mengikuti pengaturan sistem (terang/gelap)
+      routes: {
+        "/login_register_page": (context) => const LoginOrRegister(),
+        "/home_page": (context) => const HomePage(),
+        "/profile_page": (context) => const ProfilePage(),
+        "/user_page": (context) => const UserPage(),
+      },
     );
   }
 }
