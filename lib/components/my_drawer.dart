e97d697d568e392ac7553a1506975a8144e66354
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-29 09:44:17 +0800

    feat : add total drawer title

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -7,6 +7,25 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          // drawer header
+          DrawerHeader(
+            child: Icon(Icons.favorite),
+          ),
+
+          // home tile
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("H O M E"),
+            onTap: () {},
+          )
+
+          // profile user
+
+          // users title
+        ],
+      ),
     );
   }
 }
