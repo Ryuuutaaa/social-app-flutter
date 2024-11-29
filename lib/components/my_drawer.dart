cea14d239d90a51716c4fcb7faa2354f13e046ab
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-29 10:00:58 +0800

    feat : adjust drawer

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -8,31 +8,93 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // drawer header
-          DrawerHeader(
-            child: Icon(
-              Icons.favorite,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
+          Column(
+            children: [
+              // drawer header
+              DrawerHeader(
+                child: Icon(
+                  Icons.favorite,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // home tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text("H O M E"),
+                  onTap: () {
+                    // this is already the home screen so just pop drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // profile user
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text("P R O F I L E"),
+                  onTap: () {
+                    // this is already the home screen so just pop drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              const SizedBox(height: 25.0),
+
+              // users title
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.group,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text("U S E R S"),
+                  onTap: () {
+                    // this is already the home screen so just pop drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
           ),
 
-          // home tile
-          ListTile(
-            leading: Icon(
-              Icons.home,
-              color: Theme.of(context).colorScheme.inversePrimary,
+          // logout tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+            child: ListTile(
+              leading: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              title: const Text("L O G O U T"),
+              onTap: () {
+                // this is already the home screen so just pop drawer
+                Navigator.pop(context);
+              },
             ),
-            title: Text("H O M E"),
-            onTap: () {
-              // this is already the home screen so just pop drawer
-              Navigator.pop(context);
-            },
-          )
-
-          // profile user
-
-          // users title
+          ),
         ],
       ),
     );
