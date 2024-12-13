eec8ced27d4de2efc36b5d54bfb9e76dc18acaf9
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-12-13 09:39:08 +0800

    feat : add code profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  // current logged in use
+  final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  // future to fetch user details
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance.collection("Users").doc(currentUser!.email).get();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        elevation: 0,
       ),
     );
   }
